482d08d845c5890c338b3632b9ec5dfc8e4f1395
germanheinz <heinz.german@gmail.com>
2020-06-09 10:03:50 +0200

    segmenting the project into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { TodoModule } from './todos/todo.module';
 
 @NgModule({
   declarations: [
